e2c725263fea8d025c855c92d4ba2d7164f210c3
swansavage@github.com <swansavage@gmail.com>
2023-11-29 17:21:30 -0600

    try three

--- remix.config.js
+++ remix.config.js
@@ -1,10 +1,11 @@
-import { config } from "@netlify/remix-adapter";
+import { config } from '@netlify/remix-adapter';
 
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
-  ...(process.env.NODE_ENV === "production" ? config : undefined),
-  // This works out of the box with the Netlify adapter, but you can
-  // add your own custom config here if you want to.
-  //
-  // See https://remix.run/file-conventions/remix-config
+	...(process.env.NODE_ENV === 'production' ? config : undefined),
+	serverDependenciesToBundle: [/^react-icons/],
+	// This works out of the box with the Netlify adapter, but you can
+	// add your own custom config here if you want to.
+	//
+	// See https://remix.run/file-conventions/remix-config
 };
