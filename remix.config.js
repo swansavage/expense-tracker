4cae76be53a89436a712edad8914c48ac554ccc6
swansavage@github.com <swansavage@gmail.com>
2023-11-30 09:04:00 -0600

    remix config

--- remix.config.js
+++ remix.config.js
@@ -8,4 +8,13 @@ export default {
 	// add your own custom config here if you want to.
 	//
 	// See https://remix.run/file-conventions/remix-config
+	ignoredRouteFiles: ['**/.*'],
+	future: {
+		v2_dev: true,
+		v2_errorBoundary: true,
+		v2_headers: true,
+		v2_meta: true,
+		v2_normalizeFormMethod: true,
+		v2_routeConvention: true,
+	},
 };
